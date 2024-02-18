afa787acd72760f05abc3d03fc64fdd93a6aee89
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2024-02-18 16:30:52 +0100

    Actualizar BD (de Demo a Rommel)

--- mysql/init/db.sql
+++ mysql/init/db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE sistema_tickets;
-CREATE TABLE tickets (
+-- CREATE DATABASE sistema_tickets;                   # es creada y seleccionada en otros archivos
+CREATE TABLE db1.tickets (
   id INT AUTO_INCREMENT PRIMARY KEY,
   departamento VARCHAR(255) NOT NULL,
   tipo_incidencia VARCHAR(255) NOT NULL,
@@ -12,18 +12,18 @@ CREATE TABLE tickets (
   usuario_asignado INT,
   archivos VARCHAR(255)
 );
-CREATE TABLE usuarios (
+CREATE TABLE db1.usuarios (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   rol VARCHAR(255) NOT NULL
 );
-INSERT INTO tickets (departamento, tipo_incidencia, estado, fecha_creacion, fecha_actualizacion, prioridad, descripcion, usuario_creacion)
+INSERT INTO db1.tickets (departamento, tipo_incidencia, estado, fecha_creacion, fecha_actualizacion, prioridad, descripcion, usuario_creacion)
 VALUES
   ('Soporte técnico', 'Hardware', 'Abierto', '2023-11-14', '2023-11-14', 'Urgente', 'No funciona el equipo', 1),
   ('Ventas', 'Software', 'Cerrado', '2023-11-15', '2023-11-15', 'Normal', 'Problema con la licencia', 2);
-INSERT INTO usuarios (nombre, email, password, rol)
+INSERT INTO db1.usuarios (nombre, email, password, rol)
 VALUES
   ('Juan Pérez', 'juan.perez@ejemplo.com', '123456', 'Técnico'),
   ('María López', 'maria.lopez@ejemplo.com', 'abcdef', 'Jefe de IT');
--- mysql/init/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `db1`.`users` (
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `user` VARCHAR(25) NOT NULL ,
-    `password` CHAR(255) NOT NULL ,
-    `reg_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-    PRIMARY KEY (`id`), UNIQUE (`user`)
-) ENGINE = InnoDB;
-
